e33d6a33502beaf7ce7947585329ccc3633ea3fc
MichelepioBombai <michelebombai@icloud.com>
2023-04-12 09:30:47 +0200

    add migration/seeder

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,12 @@ Route::get('/', [GuestHomeController::class, 'index']);
 
 Route::get('/dashboard', [AdminHomeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')
+    ->prefix('/admin')
+    ->group(function() {
+        route::resource('projects', ProjectController::class);
+    });
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
